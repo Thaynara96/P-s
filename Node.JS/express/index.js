9bfbafe2abf087c0dbbb2230182b46f97e1d32d4
Thaynara Pimentel <thaynarapimentel@gmail.com>
2024-11-25 15:10:37 -0300

    node

---
+++ Node.JS/express/index.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const app = express ()
+
+app.get  ('/', (req, res, next) => {
+    console.log (new Date().toLocaleDateString(), req.method, req.path)
+
+    next()
+})
+
+app.get ('/', (req, res) => {
+    res.send ('Hello Word!')
+})
+
+app.get('*', (req, res) =>{
+    res.status (404).send ('Recurso não foi encontrado')
+})
+
+app.listen (3000, () => {
+    console.log ('Server is running on http://localhost:3000')
+})
