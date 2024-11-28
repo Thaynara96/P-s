cc3b278185492d5cd43b391b66e9da933ae2b749
Thaynara Pimentel <thaynarapimentel@gmail.com>
2024-11-28 15:50:11 -0300

    ToDo

---
+++ React/app-03/src/components/TodoList.js
@@ -0,0 +1,41 @@
+import { TodoItem } from "./TodoItem";
+import { useState } from "react";
+
+
+const TASK_LIST =[
+    {
+        id: 1,
+        name: "Tarefa 1",
+        isComplete: false
+    },
+
+    {
+        id: 2,
+        name: "Tarefa 2",
+        isComplete: false
+    },
+
+    {
+        id: 3,
+        name: "Tarefa 3",
+        isComplete: false
+    },
+
+    {
+        id: 4,
+        name: "Tarefa 4",
+        isComplete: false
+    },
+];
+
+export function TodoList() {
+    const [tasks, setTasks] = useState(TASK_LIST);
+
+    return (
+        <section className="task-list-container">
+            <ul>
+                {tasks.map((task) =><li><TodoItem task={task} /></li>)}
+            </ul>
+        </section>
+    )
+}
